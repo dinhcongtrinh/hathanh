f277dce2b945b951bab081fa8ec4e8b339421311
dinhcongtrinh <congtrinhdc@gmail.com>
2025-03-03 09:14:44 +0700

    hasrouter
    app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './component/navbar/Navbar';
-import { useNavigate, Routes, Route } from 'react-router-dom';  // ❌ Xóa Router, chỉ giữ Routes và Route
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from './view/Home/home';
 import New from './view/New/new';
 import Footer from './component/Footer/footer';
 import ContactPage from './view/Contact/contact.jsx';
-import RegistrationForm from './view/ContactForm/RegistrationForm.jsx';  
-
+import RegistrationForm from './view/ContactForm/RegistrationForm.jsx';
 import './App.css';
 
 function App() {
-  const navigate = useNavigate();
-
-useEffect(() => {
-  if (window.location.pathname === "/") {
-    navigate("/");
-  }
-}, [navigate]);
-
   return (
-    <>
+    <Router>
       <div className="App">
         <Navbar />
-        {/* Các thành phần khác của ứng dụng */}
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -35,11 +25,9 @@ useEffect(() => {
             <Route path="*" element={<Home />} />
           </Routes>
         </main>
-        xx
       </div>
       <Footer />
-      {/* Đặt Footer ngoài .App để không bị giới hạn chiều rộng */}
-    </>
+    </Router>
   );
 }
 
