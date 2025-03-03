250e04c55e12bfdef1ea69fecbc577e19e1def20
dinhcongtrinh <congtrinhdc@gmail.com>
2025-03-03 13:52:38 +0700

    home
    /home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navbar from './component/navbar/Navbar';
-import { useNavigate, Routes, Route } from 'react-router-dom';  // ❌ Xóa Router, chỉ giữ Routes và Route
+import { Routes, Route } from 'react-router-dom';  
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from './view/Home/home';
@@ -12,19 +12,10 @@ import RegistrationForm from './view/ContactForm/RegistrationForm.jsx';
 import './App.css';
 
 function App() {
-  const navigate = useNavigate();
-
-useEffect(() => {
-  if (window.location.pathname === "/") {
-    navigate("/");
-  }
-}, [navigate]);
-
   return (
     <>
       <div className="App">
         <Navbar />
-        {/* Các thành phần khác của ứng dụng */}
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -37,10 +28,8 @@ useEffect(() => {
         </main>
       </div>
       <Footer />
-      {/* Đặt Footer ngoài .App để không bị giới hạn chiều rộng */}
     </>
   );
 }
 
 export default App;
-
